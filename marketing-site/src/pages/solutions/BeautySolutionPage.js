e033a97c28b7334e8cd87d936184e4b7e0a51142
ProClaw Dev <dev@proclaw.cc>
2026-06-24 18:59:48 +0800

    fix: 删除 marketing-site 中重复的 .js 文件
    Vite 构建要求 JSX 文件使用 .jsx 或 .tsx 扩展名，删除所有包含 JSX 语法的 .js 文件（已有对应的 .tsx 文件）

--- marketing-site/src/pages/solutions/BeautySolutionPage.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = require("react");
-var SolutionPage_1 = require("../../components/SolutionPage");
-var solutionData_1 = require("../../lib/solutionData");
-var BeautySolutionPage = function () {
-    return <SolutionPage_1.default data={solutionData_1.solutionData.beauty} routeKey="solutionsBeauty"/>;
-};
-exports.default = BeautySolutionPage;
